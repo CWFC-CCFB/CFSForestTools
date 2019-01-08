08187caedd25b6efdb9c7d879880d4115f712be2
donenrique <donenrique@272ddc98-1990-4f49-b051-8623b4ef7cb1>
2019-01-08 09:50:02 +0000

    Some predictors moving in from REpicea.jar

---
+++ trunk/test/src/quebecmrnfutility/predictor/thinners/officialharvestmodule/OfficialHarvestableStandImpl.java
@@ -0,0 +1,59 @@
+package quebecmrnfutility.predictor.thinners.officialharvestmodule;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import quebecmrnfutility.predictor.thinners.officialharvestmodule.OfficialHarvestModel.TreatmentType;
+
+public class OfficialHarvestableStandImpl implements OfficialHarvestableStand {
+
+	private double numberOfStemsHa;
+	private Collection<OfficialHarvestableTree> trees;
+	private TreatmentType treatment;
+	
+	
+	/**
+	 * Constructor
+	 * @param numberOfStemsHa number of stems per hectare
+	 */
+	protected OfficialHarvestableStandImpl(double numberOfStemsHa, String treatment) {
+		this.numberOfStemsHa = numberOfStemsHa;
+		this.treatment = TreatmentType.valueOf(treatment.toUpperCase().trim());
+		trees = new ArrayList<OfficialHarvestableTree>();
+	}
+	
+	protected void addTree(OfficialHarvestableTree tree) {
+		trees.add(tree);
+	}
+
+	protected TreatmentType getTreatment() {return treatment;}
+	
+	protected Collection<OfficialHarvestableTree> getTrees() {return trees;}
+	
+	@Override
+	public String getSubjectId() {return ((Integer) hashCode()).toString();}
+
+	@Override
+	public int getMonteCarloRealizationId() {					// no need
+		return 0;
+	}
+
+	@Override
+	public double getNumberOfStemsHa() {
+		return this.numberOfStemsHa;
+	}
+
+	@Override
+	public double getBasalAreaM2Ha() {		
+		return 0;
+	}
+
+	/*
+	 * Useless for the tests.
+	 */
+	@Override
+	public String getPotentialVegetation() {
+		return null;
+	}
+	
+}
