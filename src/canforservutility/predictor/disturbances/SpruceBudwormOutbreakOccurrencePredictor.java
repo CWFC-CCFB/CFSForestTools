a2b6707c9e4af8793ce0bd78879082a3428c639d
donenrique <50317186+donenrique24@users.noreply.github.com>
2019-04-01 09:06:43 +0000

    Refactoring in SpruceBudwormOutbreakOccurrencePredictor

--- src/canforservutility/predictor/disturbances/SpruceBudwormOutbreakOccurrencePredictor.java
+++ src/canforservutility/predictor/disturbances/SpruceBudwormOutbreakOccurrencePredictor.java
@@ -21,72 +21,133 @@
 package canforservutility.predictor.disturbances;
 
 import java.security.InvalidParameterException;
+import java.util.HashMap;
+import java.util.Map;
 
 import repicea.simulation.REpiceaBinaryEventPredictor;
-import repicea.simulation.covariateproviders.standlevel.NaturalDisturbanceInformationProvider;
-import repicea.stats.distributions.utility.GaussianUtility;
+import repicea.stats.distributions.GammaFunction;
 
 /**
- * This class implements a simple occurrence model for spruce budworm outbreaks based on the recurrence as 
- * estimated by Boulanger and Arsenault (2004). According to their paper (p. 1041), the mean recurrence time 
- * was estimated at 39.5 years with a standard deviation of 8.6. This class does not implement stochastic features for the 
- * parameters. However is does for the residual error.
+ * This class implements an occurrence model based on a lifetime analyse for spruce budworm outbreaks based 
+ * on the recurrence as estimated by Boulanger and Arsenault (2004). According to their paper (p. 1041), 
+ * the mean recurrence time was estimated at 39.5 years with a standard deviation of 8.6. The actual model relies
+ * on a Weibull-distributed time of occurrence. 
+ * This class does not implement stochastic features for the parameters. However is does for the residual error.
  * @author Mathieu Fortin - March 2019
  * @see <a href=https://doi.org/10.1139/X03-269> Boulanger, Y., and D. Arseneault. 2004. Spruce budworm
  * outbreaks in eastern Quebec over the last 450 years. Canadian Journal of Forest Research 34: 1035-1043 
  * </a>
  */
 @SuppressWarnings("serial")
-public class SpruceBudwormOutbreakOccurrencePredictor extends REpiceaBinaryEventPredictor<NaturalDisturbanceInformationProvider, Object> {
+public final class SpruceBudwormOutbreakOccurrencePredictor extends REpiceaBinaryEventPredictor<SpruceBudwormOutbreakOccurrencePlot, Object> {
 
 	private final double mean = 39.5;
-	private final double sd = 8.6;
-	private final double truncationFactor;
-	
+//	private final double sd = 8.6;
+	private final double beta = 5.285;	// found iteratively based on a Weibull distributed time of occurrence
+	private final double lambda = 1d / mean * GammaFunction.gamma(1 + 1d/beta);	
+
+	private final Map<Integer, Map<Integer, Map<Integer, Boolean>>>  recorderMapForKnownLastOccurrence; // Monte Carlo id / current date / time since last occurrence
+	private final Map<Integer, Map<Integer, Map<Integer, Boolean>>>  recorderMapForUnknownLastOccurrence; 
 	
 	/**
-	 * Constructor. 
+	 * Deterministic constructor. 
 	 */
 	public SpruceBudwormOutbreakOccurrencePredictor() {
-		super(false, false, false);
-		truncationFactor = 1d / (1 - GaussianUtility.getCumulativeProbability(-mean/sd));
+		this(false);
 	}
-	
+
+	/**
+	 * Stochastic constructor. 
+	 */
+	protected SpruceBudwormOutbreakOccurrencePredictor(boolean isResidualVariabilityEnabled) {
+		super(false, false, isResidualVariabilityEnabled); // false : no random effect in this model	// TODO implement the stochastic features for the parameter estimates
+		recorderMapForKnownLastOccurrence = new HashMap<Integer, Map<Integer, Map<Integer, Boolean>>>();
+		recorderMapForUnknownLastOccurrence = new HashMap<Integer, Map<Integer, Map<Integer, Boolean>>>();
+	}
+
 	@Override
-	public double predictEventProbability(NaturalDisturbanceInformationProvider stand, Object tree, Object... parms) {
+	public double predictEventProbability(SpruceBudwormOutbreakOccurrencePlot plotSample, Object tree, Object... parms) {
 		if (parms != null && parms.length > 0 && parms[0] instanceof Integer) {
 			int currentDateYrs = (Integer) parms[0];
-			Integer timeSinceLastOutbreak = stand.getTimeSinceLastDisturbanceYrs(currentDateYrs);
+			Integer timeSinceLastOutbreak = plotSample.getTimeSinceLastDisturbanceYrs(currentDateYrs);
 			if (timeSinceLastOutbreak == null) {		// here we have to calculate the marginal probability
 				double marginalProb = 0d;
 				int max = 79;
-				double truncationFactor = 1d / (1 - getCumulativeProbability(stand.getTimeSinceFirstKnownDateYrs(currentDateYrs)));
-				for (int time = stand.getTimeSinceFirstKnownDateYrs(currentDateYrs) + 1; time <= max; time++) {	// marginalized over all the possible values under the assumption that all the years are equally possible
-					marginalProb += getConditionalAnnualProbability(time) * (getCumulativeProbability(time) -  getCumulativeProbability(time - 1)) * truncationFactor;
+				double truncationFactor = 1d / getSurvivorFunctionResult(plotSample.getTimeSinceFirstKnownDateYrs(currentDateYrs));
+				for (int time = plotSample.getTimeSinceFirstKnownDateYrs(currentDateYrs) + 1; time <= max; time++) {	// marginalized over all the possible values 
+					double marginalProbability = getSurvivorFunctionResult(time - 1) -  getSurvivorFunctionResult(time);
+					marginalProb += getConditionalAnnualProbabilityofOccurrence(time) * marginalProbability * truncationFactor;
 				}
 				return marginalProb;
 			}
-
-			return getConditionalAnnualProbability(timeSinceLastOutbreak);
+			return getConditionalAnnualProbabilityofOccurrence(timeSinceLastOutbreak);
 		} else {
 			throw new InvalidParameterException("The first parameter should be an integer which represents the current date (yrs)");
 		}
 	}
 
-	private double getConditionalAnnualProbability(Integer timeSinceLastOutbreak) {
-		double cumProbz0 = getCumulativeProbability(timeSinceLastOutbreak - 1);
-		double survivalFactor = 1d / (1 - cumProbz0);
-		double cumProbz1 = getCumulativeProbability(timeSinceLastOutbreak);
-		double probability = cumProbz1 - cumProbz0;
-		probability *= survivalFactor;
+	
+	private double getConditionalAnnualProbabilityofOccurrence(int timeSinceLastOutbreak) {
+		double s0 = getSurvivorFunctionResult(timeSinceLastOutbreak - 1);
+		double s1 = getSurvivorFunctionResult(timeSinceLastOutbreak);
+		double probability = 1 - s1/s0;
 		return probability;
 	}
 	
-	protected double getCumulativeProbability(int time) {
-		double z = (time - mean) / sd;
-		return GaussianUtility.getCumulativeProbability(z) * truncationFactor;
+	protected double getSurvivorFunctionResult(int time) {
+		double r = Math.exp(-Math.pow(lambda * time, beta));
+		return r;
 	}
 	
+	
+	public Object predictEvent(SpruceBudwormOutbreakOccurrencePlot plotSample, Object tree, Object... parms) {
+		double eventProbability = predictEventProbability(plotSample, tree, parms);
+		if (eventProbability < 0 || eventProbability > 1) {
+			return null;
+		} else if (isResidualVariabilityEnabled) {
+			int currentDateYrs = (Integer) parms[0];
+			Integer timeSinceLastOutbreak = plotSample.getTimeSinceLastDisturbanceYrs(currentDateYrs);
+			if (timeSinceLastOutbreak == null) {
+				int timeSinceFirstKnownDate = plotSample.getTimeSinceFirstKnownDateYrs(currentDateYrs);
+				return getResidualError(recorderMapForUnknownLastOccurrence,
+						plotSample.getMonteCarloRealizationId(), 
+						currentDateYrs, 
+						timeSinceFirstKnownDate, 
+						eventProbability);
+			} else {
+				return getResidualError(recorderMapForKnownLastOccurrence,
+						plotSample.getMonteCarloRealizationId(), 
+						currentDateYrs, 
+						timeSinceLastOutbreak, 
+						eventProbability);
+			}
+		} else {
+			return eventProbability;
+		}
+	}
+
+	
+	
+	private synchronized boolean getResidualError(Map<Integer, Map<Integer, Map<Integer, Boolean>>> oMap,
+			int monteCarloRealization, 
+			int currentDate, 
+			int timeSince, 
+			double probability) {
+		if (!oMap.containsKey(monteCarloRealization)) {
+			oMap.put(monteCarloRealization, new HashMap<Integer, Map<Integer, Boolean>>());
+		}
+		Map<Integer, Map<Integer, Boolean>> innerMap = oMap.get(monteCarloRealization);
+		if (!innerMap.containsKey(currentDate)) {
+			innerMap.put(currentDate, new HashMap<Integer, Boolean>());
+		}
+		Map<Integer, Boolean> innerInnerMap = innerMap.get(currentDate);
+		if (!innerInnerMap.containsKey(timeSince)) {
+			double residualError = random.nextDouble();
+			innerInnerMap.put(timeSince, residualError < probability);
+		}
+		return innerInnerMap.get(timeSince);
+	}
+
 	/*
 	 * Useless for this class (non-Javadoc)
 	 * @see repicea.simulation.REpiceaPredictor#init()
@@ -94,4 +155,5 @@ public class SpruceBudwormOutbreakOccurrencePredictor extends REpiceaBinaryEvent
 	@Override
 	protected void init() {}
 
+	
 }
