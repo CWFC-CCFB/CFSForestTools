70eeed160c4f49b26fc3132912c6e7e85c86b427
donenrique <donenrique@272ddc98-1990-4f49-b051-8623b4ef7cb1>
2019-10-23 15:35:42 +0000

    Refactoring multiple-site beta diversity indices

--- trunk/manuscripts/canforservutility/biodiversity/indices/PopulationTestsRealData.java
+++ trunk/manuscripts/canforservutility/biodiversity/indices/PopulationTestsRealData.java
@@ -1,14 +1,22 @@
 package canforservutility.biodiversity.indices;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import canforservutility.biodiversity.indices.DiversityIndices.BetaIndex;
+import repicea.io.FormatField;
+import repicea.io.javacsv.CSVField;
 import repicea.io.javacsv.CSVReader;
+import repicea.io.javacsv.CSVWriter;
+import repicea.serial.xml.XmlSerializer;
+import repicea.stats.sampling.SamplingUtility;
 import repicea.util.ObjectUtility;
 
+@SuppressWarnings("serial")
 public class PopulationTestsRealData {
 
 	private class Plot extends ArrayList<String> {
@@ -42,11 +50,90 @@ public class PopulationTestsRealData {
 				plot.add(species);
 			} 
 		}
+		reader.close();
 		return populations;
 	}
 
-	public static void main(String[] args) throws IOException {
-		PopulationTestsRealData obj = new PopulationTestsRealData();
-		Map<String, Population> populations = obj.readData();
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private Object[] testThisSubPopulation(Population originalPopulation, int populationSize, String suffix) throws Exception {
+		Map pop = SamplingUtility.getSample(originalPopulation, populationSize);
+		if (populationSize == 1000) {
+			XmlSerializer serializer = new XmlSerializer(ObjectUtility.getPackagePath(PopulationTests.class).replace("bin", "manuscripts") + "pop" + suffix + populationSize);
+			serializer.writeObject(pop);
+		}
+		MultipleSiteIndex msi = new MultipleSiteIndex();
+		DiversityIndices currentIndices = msi.getMultiplesiteDissimilarityIndices(pop);
+		DiversityIndices newIndices = msi.getAdaptedMultiplesiteDissimilarityIndices(pop);
+		Object[] record = new Object[10];
+		record[0] = populationSize;
+		record[1] = newIndices.getGammaDiversity();
+		record[2] = newIndices.getAlphaDiversity();
+		record[3] = suffix;
+		record[4] = currentIndices.getBetaIndex(BetaIndex.Simpson);
+		record[5] = currentIndices.getBetaIndex(BetaIndex.Sorensen);
+		record[6] = currentIndices.getBetaIndex(BetaIndex.Nestedness);
+		record[7] = newIndices.getBetaIndex(BetaIndex.Simpson);
+		record[8] = newIndices.getBetaIndex(BetaIndex.Sorensen);
+		record[9] = newIndices.getBetaIndex(BetaIndex.Nestedness);
+		return record;
+	}
+
+	
+	
+	public static void main(String[] args) throws Exception {
+		String rootPath = ObjectUtility.getPackagePath(PopulationTests.class).replace("bin", "manuscripts");
+		String filename;
+		CSVWriter writer;
+		List<FormatField> fields;
+		
+		
+		filename =  rootPath + "populationRealDataTests.csv";
+		writer = new CSVWriter(new File(filename), false);
+		fields = new ArrayList<FormatField>();
+		fields.add(new CSVField("PopSize"));
+		fields.add(new CSVField("speciesRichness"));
+		fields.add(new CSVField("averageSpecies"));
+		fields.add(new CSVField("bioClimDomain"));
+		fields.add(new CSVField("Simpson"));
+		fields.add(new CSVField("Sorensen"));
+		fields.add(new CSVField("Nestedness"));
+		fields.add(new CSVField("SimpsonCorr"));
+		fields.add(new CSVField("SorensenCorr"));
+		fields.add(new CSVField("NestednessCorr"));
+		writer.setFields(fields);
+		
+		List<Integer> populationSizes = new ArrayList<Integer>();
+		populationSizes.add(20);
+		populationSizes.add(30);
+		populationSizes.add(40);
+		populationSizes.add(50);
+		populationSizes.add(60);
+		populationSizes.add(70);
+		populationSizes.add(80);
+		populationSizes.add(90);
+		populationSizes.add(100);
+		populationSizes.add(200);
+		populationSizes.add(300);
+		populationSizes.add(400);
+		populationSizes.add(500);
+		populationSizes.add(600);
+		populationSizes.add(700);
+		populationSizes.add(800);
+		populationSizes.add(900);
+		populationSizes.add(1000);
+
+		PopulationTestsRealData instance = new PopulationTestsRealData();
+		Map<String, Population> populations = instance.readData();
+		for (String bioClimDomain : populations.keySet()) {
+			Population originalPopulation = populations.get(bioClimDomain);
+			if (originalPopulation.size() > 1000) {
+				for (Integer populationSize : populationSizes) {
+					System.out.println("Simulation population size " + populationSize + " with bioclimatic domain = " + bioClimDomain);
+					Object[] record = instance.testThisSubPopulation(originalPopulation, populationSize, bioClimDomain);
+					writer.addRecord(record);
+				}
+			}
+		}
+		writer.close();
 	}
 }
