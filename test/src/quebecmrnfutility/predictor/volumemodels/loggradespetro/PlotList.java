d416618fc23ce8381a4071672f64dc56e7b63fd6
donenrique <50317186+donenrique24@users.noreply.github.com>
2021-01-22 15:49:12 +0000

    Adding a build.xml file for ant compilation

---
+++ test/src/quebecmrnfutility/predictor/volumemodels/loggradespetro/PlotList.java
@@ -0,0 +1,33 @@
+package quebecmrnfutility.predictor.volumemodels.loggradespetro;
+
+import java.util.ArrayList;
+
+import repicea.math.Matrix;
+import repicea.stats.estimates.PopulationTotalEstimate;
+import repicea.stats.sampling.PopulationUnitWithUnequalInclusionProbability;
+
+@SuppressWarnings("serial")
+class PlotList extends ArrayList<Plot> {
+
+	void setRealization(int id) {
+		for (Plot plot : this) {
+			for (PetroGradeTreeImpl tree : plot.getTrees()) {
+				tree.setRealization(id);
+			}
+		}
+	}
+	
+	PopulationTotalEstimate getHorvitzThompsonEstimate(int populationSize) {
+		PopulationTotalEstimate estimate = new PopulationTotalEstimate();
+		
+		for (Plot plot : this) {
+			Matrix plotTotal = new Matrix(5,1);
+			for (PetroGradeTreeImpl tree : plot.getTrees()) {
+				plotTotal = plotTotal.add(tree.getRealizedValues());
+			}
+			estimate.addObservation(new PopulationUnitWithUnequalInclusionProbability(plotTotal, 1d/populationSize));
+		}
+		return estimate;
+	}
+	
+}
