bb9147b980bca80c825a123fb75756498e17dac3
jflavoie <jflavoie@272ddc98-1990-4f49-b051-8623b4ef7cb1>
2021-09-16 19:11:40 +0000

    Added missing ProductionEnvironmentTest.java

--- test/src/ProductionEnvironmentTest.java
+++ test/src/ProductionEnvironmentTest.java
@@ -11,7 +11,7 @@ public class ProductionEnvironmentTest {
 	 * This test will fail when used with a local class folder, but will succeed when running on a JAR file.
 	 * This test is excluded from the normal local tests and executed only with the integration tests from the JAR file
 	 */
-	public void makeSureRepiceaIsRunningFromJAR() {
+	public void makeSurePackageIsRunningFromJAR() {
 		String resourceURL = Lambert2005BiomassPredictor.class.getResource("Lambert2005BiomassPredictor.class").toString();
 		Assert.assertTrue(resourceURL.startsWith("jar:"));
 	}
