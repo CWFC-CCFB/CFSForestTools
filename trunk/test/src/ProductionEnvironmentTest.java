a9066215e5626d0bcd2e86027ac6c50e80008fbf
jflavoie <jflavoie@272ddc98-1990-4f49-b051-8623b4ef7cb1>
2021-09-16 18:42:02 +0000

    Added ProductionEnvironmentTest.java to test if production tests are really running from JAR package

---
+++ trunk/test/src/ProductionEnvironmentTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import canforservutility.predictor.biomass.lambert2005.Lambert2005BiomassPredictor;
+
+
+public class ProductionEnvironmentTest {
+	@Test 
+	
+	/**
+	 * This test will fail when used with a local class folder, but will succeed when running on a JAR file.
+	 * This test is excluded from the normal local tests and executed only with the integration tests from the JAR file
+	 */
+	public void makeSureRepiceaIsRunningFromJAR() {
+		String resourceURL = Lambert2005BiomassPredictor.class.getResource("Lambert2005BiomassPredictor.class").toString();
+		Assert.assertTrue(resourceURL.startsWith("jar:"));
+	}
+}
