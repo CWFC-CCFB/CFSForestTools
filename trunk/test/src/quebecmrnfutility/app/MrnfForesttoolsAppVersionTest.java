1fa198068ca509b7afa4dd663949f787c6f96241
donenrique <donenrique@272ddc98-1990-4f49-b051-8623b4ef7cb1>
2021-10-04 23:08:25 +0000

    Version is now stored in revision file

--- trunk/test/src/quebecmrnfutility/app/MrnfForesttoolsAppVersionTest.java
+++ trunk/test/src/quebecmrnfutility/app/MrnfForesttoolsAppVersionTest.java
@@ -32,12 +32,20 @@ public class MrnfForesttoolsAppVersionTest {
 	public void compileAndRetrieveRevision() {
 		String build = MrnfForesttoolsAppVersion.getInstance().getBuild();
 		System.out.println("Build is: " + build);
+		String version = MrnfForesttoolsAppVersion.getInstance().getVersion();
+		System.out.println("Version is: " + version);
 		if (JarUtility.isEmbeddedInJar(MrnfForesttoolsAppVersion.class)) {
 			try {
 				Integer.parseInt(build);
 			} catch (NumberFormatException e) {
 				Assert.fail("The revision cannot be parsed to an integer!");
+			}		
+			try {
+				Double.parseDouble(version);
+			} catch (NumberFormatException e) {
+				Assert.fail("The version cannot be parsed to a double!");
 			}
+
 		} else {
 			Assert.assertEquals("Unknown", build);
 		}
