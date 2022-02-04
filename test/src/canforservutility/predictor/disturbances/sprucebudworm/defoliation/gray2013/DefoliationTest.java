cdfd46cb14c39d9c9ea220c3a28804034415f2e6
donenrique <50317186+donenrique24@users.noreply.github.com>
2022-02-04 16:08:22 +0000

    Adapting to comply with the new version of biosimclient

--- test/src/canforservutility/predictor/disturbances/sprucebudworm/defoliation/gray2013/DefoliationTest.java
+++ test/src/canforservutility/predictor/disturbances/sprucebudworm/defoliation/gray2013/DefoliationTest.java
@@ -1,14 +1,27 @@
 package canforservutility.predictor.disturbances.sprucebudworm.defoliation.gray2013;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import biosimclient.BioSimClient;
 import biosimclient.BioSimException;
 import repicea.math.Matrix;
 
 public class DefoliationTest {
 
+	@BeforeClass
+	public static void initializeTest() {
+		BioSimClient.isLocal = true;
+	}
+	
+	@AfterClass
+	public static void finalizeTest() {
+		BioSimClient.isLocal = false;
+	}
+	
 	@Test
 	public void simpleTestWithoutBioSIM() throws BioSimException {
 		DefoliationPredictor pred = new DefoliationPredictor(4);
@@ -28,37 +41,36 @@ public class DefoliationTest {
 	}
 	
 	// This GrayDefoliator class must be improved. It is simply too long.
-	@Ignore 
 	@Test
 	public void simpleTestWithBioSIM() throws BioSimException {
 		DefoliationPredictor pred = new DefoliationPredictor(4);
 		DefoliationPlot plot = new DefoliationPlotImpl(48d, -70d, 350d, 15, 70, 1d, 1986);
 		Matrix resultingEstimate = pred.getDurationAndSeverityEstimate(plot);
 		double durationYrs = resultingEstimate.getValueAt(0, 0);
-		Assert.assertEquals("Testing duration (yrs)", 19.408770208511704, durationYrs, 0.2);
+		Assert.assertEquals("Testing duration (yrs)", 19.927, durationYrs, 0.2);
 		double severity = resultingEstimate.getValueAt(1, 0);
 		Assert.assertEquals("Testing severity (%)", 8.942412817277273, severity, 8);
 		System.out.println("Duration = " + durationYrs + "; severity = " + severity);
 		plot = new DefoliationPlotImpl(51d, -70d, 450d, 70, 15, 1d, 1986);
 		resultingEstimate = pred.getDurationAndSeverityEstimate(plot);
 		durationYrs = resultingEstimate.getValueAt(0, 0);
-		Assert.assertEquals("Testing duration (yrs)", 9.538209491956952, durationYrs, 0.2);
+		Assert.assertEquals("Testing duration (yrs)", 9.282, durationYrs, 0.2);
 		severity = resultingEstimate.getValueAt(1, 0);
-		Assert.assertEquals("Testing severity (%)", 59.20383493012326, severity, 11);
+		Assert.assertEquals("Testing severity (%)", 15.756, severity, 11);
 		System.out.println("Duration = " + durationYrs + "; severity = " + severity);
 		
 		
-		plot = new DefoliationPlotImpl(49d, -70d, 450d, 0, 100, 1d, 1986);
-		resultingEstimate = pred.getDurationAndSeverityEstimate(plot);
-		durationYrs = resultingEstimate.getValueAt(0, 0);
-		severity = resultingEstimate.getValueAt(1, 0);
-		System.out.println("Duration = " + durationYrs + "; severity = " + severity);
-
-		plot = new DefoliationPlotImpl(47d, -71d, 600d, 0, 100, 1d, 1986);
-		resultingEstimate = pred.getDurationAndSeverityEstimate(plot);
-		durationYrs = resultingEstimate.getValueAt(0, 0);
-		severity = resultingEstimate.getValueAt(1, 0);
-		System.out.println("Duration = " + durationYrs + "; severity = " + severity);
+//		plot = new DefoliationPlotImpl(49d, -70d, 450d, 0, 100, 1d, 1986);
+//		resultingEstimate = pred.getDurationAndSeverityEstimate(plot);
+//		durationYrs = resultingEstimate.getValueAt(0, 0);
+//		severity = resultingEstimate.getValueAt(1, 0);
+//		System.out.println("Duration = " + durationYrs + "; severity = " + severity);
+//
+//		plot = new DefoliationPlotImpl(47d, -71d, 600d, 0, 100, 1d, 1986);
+//		resultingEstimate = pred.getDurationAndSeverityEstimate(plot);
+//		durationYrs = resultingEstimate.getValueAt(0, 0);
+//		severity = resultingEstimate.getValueAt(1, 0);
+//		System.out.println("Duration = " + durationYrs + "; severity = " + severity);
 
 	}
 	
