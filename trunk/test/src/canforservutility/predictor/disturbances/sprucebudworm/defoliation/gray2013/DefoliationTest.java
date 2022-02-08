37376780f35fd63f471b69b035829b16d2b855e3
donenrique <donenrique@272ddc98-1990-4f49-b051-8623b4ef7cb1>
2022-02-08 15:38:28 +0000

    Adapting to comply with the new version of biosimclient

--- trunk/test/src/canforservutility/predictor/disturbances/sprucebudworm/defoliation/gray2013/DefoliationTest.java
+++ trunk/test/src/canforservutility/predictor/disturbances/sprucebudworm/defoliation/gray2013/DefoliationTest.java
@@ -1,3 +1,24 @@
+/*
+ * This file is part of the mrnf-foresttools library
+ *
+ * Copyright (C) 2021 Her Majesty the Queen in right of Canada
+ * Author: Mathieu Fortin
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package canforservutility.predictor.disturbances.sprucebudworm.defoliation.gray2013;
 
 import org.junit.AfterClass;
@@ -12,15 +33,15 @@ import repicea.math.Matrix;
 
 public class DefoliationTest {
 
-	@BeforeClass
-	public static void initializeTest() {
-		BioSimClient.setLocalConnectionEnabled(true);
-	}
-	
-	@AfterClass
-	public static void finalizeTest() {
-		BioSimClient.setLocalConnectionEnabled(false);
-	}
+//	@BeforeClass
+//	public static void initializeTest() {
+//		BioSimClient.setLocalConnectionEnabled(true);
+//	}
+//	
+//	@AfterClass
+//	public static void finalizeTest() {
+//		BioSimClient.setLocalConnectionEnabled(false);
+//	}
 	
 	@Test
 	public void simpleTestWithoutBioSIM() throws BioSimException {
