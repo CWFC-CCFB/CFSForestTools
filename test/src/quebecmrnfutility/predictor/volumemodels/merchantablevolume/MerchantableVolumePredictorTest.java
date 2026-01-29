d969d40dfe373d83eef31e08d64a246888b736a4
Mathieu Fortin <mathieu.fortin.re@gmail.com>
2026-01-29 11:54:45 -0500

    Fast-tracking volume predictions for the MerchantableVolumePredictor class. Adding unit tests. Upgrading to latest version of repicea-simulation.jar

--- test/src/quebecmrnfutility/predictor/volumemodels/merchantablevolume/MerchantableVolumePredictorTest.java
+++ test/src/quebecmrnfutility/predictor/volumemodels/merchantablevolume/MerchantableVolumePredictorTest.java
@@ -21,13 +21,17 @@
  */
 package quebecmrnfutility.predictor.volumemodels.merchantablevolume;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
+
+import repicea.simulation.species.REpiceaSpecies;
 
 public class MerchantableVolumePredictorTest {
 
 	@Test
-	public void simpleTest() {
+	public void test01SimpleHappyPath() {
 		VolumableStand p = new VolumableStandImpl();
 		VolumableTree t = new VolumableTreeImpl("BOJ", 18, 15);
 		MerchantableVolumePredictor volPred = new MerchantableVolumePredictor();
@@ -36,7 +40,7 @@ public class MerchantableVolumePredictorTest {
 	}
 
 	@Test
-	public void simpleWithNonmerchantableTree() {
+	public void test02WithNonmerchantableTree() {
 		VolumableStand p = new VolumableStandImpl();
 		VolumableTree t = new VolumableTreeImpl("BOJ", 8d, 15);
 		MerchantableVolumePredictor volPred = new MerchantableVolumePredictor();
@@ -45,12 +49,12 @@ public class MerchantableVolumePredictorTest {
 	}
 
 	@Test
-	public void simpleWithNonobservedHeight() {
+	public void test03WithNonobservedHeight() {
 		VolumableStand p = new VolumableStandImpl();
 		VolumableTree t = new VolumableTreeImpl("BOJ", 10d, -1d);
 		MerchantableVolumePredictor volPred = new MerchantableVolumePredictor();
 		try {
-			double volumeDM3 = volPred.predictTreeCommercialUnderbarkVolumeDm3(p, t);
+			volPred.predictTreeCommercialUnderbarkVolumeDm3(p, t);
 			Assert.fail("Should have thrown an exception!");
 		} catch (Exception e) {
 			
@@ -58,16 +62,75 @@ public class MerchantableVolumePredictorTest {
 	}
 
 	@Test
-	public void simpleWithTooShortTree() {
+	public void test04WithTooShortTree() {
 		VolumableStand p = new VolumableStandImpl();
 		VolumableTree t = new VolumableTreeImpl("BOJ", 10d, 1d);
 		MerchantableVolumePredictor volPred = new MerchantableVolumePredictor();
 		try {
-			double volumeDM3 = volPred.predictTreeCommercialUnderbarkVolumeDm3(p, t);
+			volPred.predictTreeCommercialUnderbarkVolumeDm3(p, t);
+			Assert.fail("Should have thrown an exception!");
+		} catch (Exception e) {}
+	}
+
+	@Test
+	public void test05FastTrackSimpleHappyPath() {
+//		VolumableStand p = new VolumableStandImpl();
+//		VolumableTree t = new VolumableTreeImpl("BOJ", 18, 15);
+		MerchantableVolumePredictor volPred = new MerchantableVolumePredictor();
+		double volumeDM3 = volPred.predictDeterministicTreeCommercialVolumeDm3("Betula alleghaniensis", 18d, 15d, false);
+		Assert.assertEquals("Testing volume", 149.284990537785, volumeDM3, 1E-8);
+	}
+
+	@Test
+	public void test06FastTrackWithNonmerchantableTree() {
+//		VolumableStand p = new VolumableStandImpl();
+//		VolumableTree t = new VolumableTreeImpl("BOJ", 8d, 15);
+		MerchantableVolumePredictor volPred = new MerchantableVolumePredictor();
+		double volumeDM3 = volPred.predictDeterministicTreeCommercialVolumeDm3("Betula alleghaniensis", 8d, 15d, false);
+		Assert.assertEquals("Testing volume", 0d, volumeDM3, 1E-8);
+	}
+
+	@Test
+	public void test07FastTrackWithNonobservedHeight() {
+//		VolumableStand p = new VolumableStandImpl();
+//		VolumableTree t = new VolumableTreeImpl("BOJ", 10d, -1d);
+		MerchantableVolumePredictor volPred = new MerchantableVolumePredictor();
+		try {
+			volPred.predictDeterministicTreeCommercialVolumeDm3("Betula alleghaniensis", 10d, -1d, false);
 			Assert.fail("Should have thrown an exception!");
 		} catch (Exception e) {
 			
 		}
+	}
+
+	@Test
+	public void test08FastTrackWithTooShortTree() {
+//		VolumableStand p = new VolumableStandImpl();
+//		VolumableTree t = new VolumableTreeImpl("BOJ", 10d, 1d);
+		MerchantableVolumePredictor volPred = new MerchantableVolumePredictor();
+		try {
+			volPred.predictDeterministicTreeCommercialVolumeDm3("Betula alleghaniensis", 10d, 1d, false);
+			Assert.fail("Should have thrown an exception!");
+		} catch (Exception e) {}
+	}
+
+	@Test
+	public void test09FastTrackSimpleHappyPathOverbark() {
+//		VolumableStand p = new VolumableStandImpl();
+//		VolumableTree t = new VolumableTreeImpl("BOJ", 18, 15);
+		MerchantableVolumePredictor volPred = new MerchantableVolumePredictor();
+		double volumeDM3 = volPred.predictDeterministicTreeCommercialVolumeDm3("Betula alleghaniensis", 18d, 15d, true);
+		Assert.assertEquals("Testing volume", 
+				149.284990537785 * (1 + REpiceaSpecies.Species.Betula_alleghaniensis_QC.getBarkProportionOfWoodVolume()),
+				volumeDM3, 
+				1E-8);
+	}
+	
+	@Test
+	public void test10LatinNameList() {
+		List<String> species = MerchantableVolumePredictor.getEligibleSpecies();
+		Assert.assertEquals("Testing the number of species", 26, species.size());
+		System.out.println(species);
 	}
 
 }
